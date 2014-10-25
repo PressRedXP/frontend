42c964d9ad224d1d2613104796f91e7dd2687434
crosslandwa <crossland.w.a@gmail.com>
2014-10-25 17:43:54 +0100

    Example ajax request to get people list. Currently outputs straight into page HTML

--- js/geolocation.js
+++ js/geolocation.js
@@ -13,3 +13,23 @@ function showPosition(position) {
     x.innerHTML="Latitude: " + position.coords.latitude + 
     "<br>Longitude: " + position.coords.longitude;	
 }
+
+function getPeopleList() {
+
+	// TODO pass in my id instead of "mrbuttons"
+    $.ajax({url:"http://justmeet-backend.herokuapp.com/people/mrbuttons/contacts",success:function(result){
+      $("#demo").html(result);
+    }});
+
+
+
+// example POST
+//	$.ajax({
+//	  type: "POST",
+//	  url: "some.php",
+//	  data: { name: "John", location: "Boston" }
+//	})
+//	  .done(function( msg ) {
+//	    alert( "Data Saved: " + msg );
+//	  });
+}
