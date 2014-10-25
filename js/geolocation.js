8222e41e803f445ebfd6054e3946cbc442b602d2
crosslandwa <crossland.w.a@gmail.com>
2014-10-25 18:45:28 +0100

    Parse JSON and add list of names to page

--- js/geolocation.js
+++ js/geolocation.js
@@ -15,12 +15,27 @@ function showPosition(position) {
 }
 
 function getPeopleList() {
-
+	var peopleListUrl = "http://justmeet-backend.herokuapp.com/people/mrbuttons/contacts";
 	// TODO pass in my id instead of "mrbuttons"
-    $.ajax({url:"http://justmeet-backend.herokuapp.com/people/mrbuttons/contacts",success:function(result){
-      $("#demo").html(result);
-    }});
+//    $.ajax({url: peopleListUrl,success:function(result){
+//      $("#demo").html(result);
+//    }});
 
+
+	$.getJSON( peopleListUrl, function( data ) {
+		var items = [];
+		
+		$.each( data.people, function( key, person ) {
+			items.push( "<li id='" + key + "' class='list-group-item'>" + person.name + "</li>" );
+		});
+		
+		$("#row-1-left").html($( "<ul/>", {
+			"class": "list-group",
+			html: items.join( "" )
+		}));
+	
+	
+	});
 
 
 // example POST
